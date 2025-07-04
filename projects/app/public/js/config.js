bad24bdfe19ebba2aca06061885901b1e98e53b9
zhangdingyu <zhangdy@soyuan.com.cn>
2025-07-04 09:33:18 +0800

    sfzy定制化-在线文档对接-编辑预览

--- projects/app/public/js/config.js
+++ projects/app/public/js/config.js
@@ -1,3 +1,5 @@
 window.myConfig = {
-  sfzyUrl: 'http://localhost:8080'
-}
+  sfzyUrl: 'http://localhost:8080',
+  docServerUrl: 'http://172.20.22.58:8008',
+  onlineDocPlatformUrl: 'http://127.0.0.1'
+};
